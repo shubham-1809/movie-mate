3f69af730c5e922984b1cdc4dbcc341536f7ba8c
shubham-1809 <shubhamchouksey1809@gmail.com>
2023-12-13 10:22:19 +0530

    updated files

--- frontend/src/components/Auth/Admin.js
+++ frontend/src/components/Auth/Admin.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { sendAdminAuthRequest } from "../../api-helpers/api-helpers";
+import { sendAdminAuthRequest } from "../../api-helpers/api-helpers.js";
 import { adminActions } from "../../store";
-import AuthForm from "./AuthForm";
+import AuthForm from "./AuthForm.js";
 
 const Admin = () => {
   const navigate = useNavigate();
